318975d408e43ccc0544472730791de51dfe1a26
ywxt <ywxtcwh@qq.com>
2023-03-13 21:51:55 +0800

    Fix detekt

--- core/src/main/kotlin/me/ywxt/langhuan/core/utils/Either.kt
+++ core/src/main/kotlin/me/ywxt/langhuan/core/utils/Either.kt
@@ -5,6 +5,7 @@ import arrow.core.left
 import arrow.core.nonFatalOrThrow
 import arrow.core.right
 
+@Suppress("TooGenericExceptionCaught")
 inline fun <R> catchException(f: () -> R): Either<Exception, R> =
     try {
         f().right()
